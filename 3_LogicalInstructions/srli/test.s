2e0b4211f5841e0b628fe9051d0aa59b104bc40e
TroyMitchell911 <andrew998@126.com>
2024-04-06 17:17:50 +0800

    Add 'srli' instruction

---
+++ 3_LogicalInstructions/srli/test.s
@@ -0,0 +1,12 @@
+.text
+.global _start   # Marks the _start label as global
+
+_start:
+    li x5, 0x80   # Load immediate: Load the immediate value 0x80 into register x5
+    srli x5, x5, 0x02   # Shift right logical immediate: Shift the value in register x5 right by the immediate value 0x02
+
+stop:
+    j stop   # Unconditionally jump to the stop label, creating an infinite loop
+
+.end   # End of the assembly code
+
